72373c5a16ba7297efc7de7b77ac07c5afe24f9d
Hampus Andreas Niklas Rosencrantz <hamros@tuta.io>
2021-12-17 14:45:41 +0100

    ...

---
+++ xcb.cr
@@ -0,0 +1,142 @@
+@[Link("XCB")]
+lib LibXCB
+	X_PROTOCOL = 11
+
+	X_PROTOCOL_REVISION = 0
+
+	X_TCP_PORT = 6000
+
+	XCB_CONN_ERROR = 1
+
+	XCB_CONN_CLOSED_EXT_NOTSUPPORTED = 2
+
+	XCB_CONN_CLOSED_MEM_INSUFFICIENT = 3
+
+	XCB_CONN_CLOSED_REQ_LEN_EXCEED = 4
+
+	XCB_CONN_CLOSED_PARSE_ERR = 5
+
+	XCB_CONN_CLOSED_INVALID_SCREEN = 6
+
+	XCB_CONN_CLOSED_FDPASSING_FAILED = 7
+
+	macro XCB_TYPE_PAD(t, i)
+		(-{{i}} & sizeof T > 4 ? 3 : sizeof T - 1)
+	end
+
+	struct GenericIterator 
+		data :    Pointer(Void)
+		rem :     Int32 
+		index :   Int32 
+	end
+
+	struct GenericReply 
+		response_type :   UInt8   
+		pad0 :            UInt8  
+		sequence :        UInt16 
+		length :          UInt32 
+	end
+
+	struct GenericEvent 
+		response_type :   UInt8   
+		pad0 :            UInt8  
+		sequence :        UInt16 
+		pad :          StaticArray(UInt32, 7)
+		full_sequence :   UInt32 
+	end
+
+	struct RawGenericEvent 
+		response_type :   UInt8   
+		pad0 :            UInt8  
+		sequence :        UInt16 
+		pad : StaticArray(UInt32, 7)           
+	end
+
+	struct GeEvent 
+		response_type :   UInt8  
+		pad0 :            UInt8  
+		sequence :        UInt16 
+		length : UInt32 
+		event_type : UInt16 
+		pad1 : UInt16 
+		pad[5] :          StaticArray(UInt32, 5)
+		full_sequence :   UInt32 
+	end
+
+	struct GenericError 
+		response_type :   UInt8   
+		error_code :      UInt8   
+		sequence :        UInt16 
+		resource_id :      UInt32 
+		minor_code :       UInt16 
+		major_code :        UInt8 
+		pad0 : UInt8 
+		pad :          StaticArray(UInt32, 5)
+		full_sequence :   UInt32 
+	end
+
+	struct VoidCookie 
+		UInt32 sequence :   
+	end
+
+	XCB_NONE = 0_i64
+
+	XCB_COPY_FROM_PARENT = 0_i64
+
+	XCB_CURRENT_TIME = 0_i64
+
+	XCB_NO_SYMBOL = 0_i64
+
+	struct AuthInfo 
+		namelen : Int32     
+		name :      Pointer(Char)
+		datalen :   Int32   
+		data :    Pointer(Char)
+	end
+
+	fun xcb_flush(x0 : Pointer(Connection)) : Int32 
+
+	fun xcb_get_maximum_request_length(x0 : Pointer(Connection)) : UInt32 
+
+	fun xcb_prefetch_maximum_request_length(x0 : Pointer(Connection)) : UInt32 
+
+	fun xcb_wait_for_event(x0 : Pointer(Connection)) : Pointer(GenericEvent)
+
+	fun xcb_poll_for_event(x0 : Pointer(Connection)) : Pointer(GenericEvent) 
+
+	fun xcb_poll_for_queued_event(x0 : Pointer(Connection) ) : Pointer(GenericEvent)
+
+	fun xcb_poll_for_special_event(x0 : Pointer(Connection), x1 : Pointer(SpecialEvent)) : Pointer(GenericEvent)
+
+	fun xcb_wait_for_special_event(x0 : Pointer(Connection), x1 : Pointer(SpecialEvent)) : Pointer(GenericEvent)
+
+	fun xcb_register_for_special_xge(x0 : Pointer(Connection) , x1 : Pointer(Extension), x2 : UInt32, x3 : Pointer(UInt32)) : Pointer(SpecialEvent)
+
+	fun xcb_unregister_for_special_event(x0 : Pointer(Connection) , x1 : Pointer(SpecialEvent)) : Void 
+
+	fun xcb_request_check(x0 : Pointer(Connection) , x1 : VoidCookie cookie) : Pointer(GenericError)
+
+	fun xcb_discard_reply(x0 : Pointer(Connection), x1 : UInt32 sequence) : Void 
+
+	fun xcb_discard_reply64(x0 : Pointer(Connection), x1 : UInt64 sequence) : Void 
+
+	type QueryExtensionReply = Pointer(xcb_get_extension_data(Pointer(Connection), Pointer(Extension)))
+
+	fun xcb_prefetch_extension_data(x0 : Pointer(Connection) , x1 : Pointer(Extension)) : Void 
+
+	type Setup = Pointer(xcb_get_setup(Pointer(Connection)))
+
+	fun xcb_get_file_descriptor(x0 : Pointer(Connection) ) : Int32 
+
+	fun xcb_connection_has_error(x0 : Pointer(Connection) ) : Int32 
+
+	fun xcb_connect_to_fd(x0 : Int32, x1 : Pointer(AuthInfo)) : Pointer(Connection)
+
+	fun cxcb_disconnect(Pointer(Connection)) : Void
+
+	fun xcb_parse_display(x0 : Pointer(Char), x1 : Pointer(Char*), x2 : Pointer(Int32), x3 : Pointer(Int32)) : Int32 
+
+	fun cb_connect(x0 : Pointer(Char), x1 : Pointer(Int32)) : Pointer(Connection)
+
+	fun cb_connect_to_display_with_auth_info(x0 : Pointer(Char), x1 : Pointer(AuthInfo), x2 : Pointer(Int32)) : Pointer(Connection)
+end
--- xcb_aux.cr
+++ xcb_aux.cr
@@ -1,102 +1,105 @@
-fun xcb_aux_get_depth(x0 : Pointer(Connection), x1 : Pointer(Screen)) : UInt8
+@[Link("XCB")]
+lib LibXCB
+	fun xcb_aux_get_depth(x0 : Pointer(Connection), x1 : Pointer(Screen)) : UInt8
 
-fun xcb_aux_get_depth_of_visual(x0 : Pointer(Screen), x1 : Visualid) :  UInt8 
+	fun xcb_aux_get_depth_of_visual(x0 : Pointer(Screen), x1 : Visualid) :  UInt8 
 
-fun xcb_aux_get_screen(x0 : Pointer(Connection), x1 : Int32) :  Pointer(Screen)
+	fun xcb_aux_get_screen(x0 : Pointer(Connection), x1 : Int32) :  Pointer(Screen)
 
-fun xcb_aux_get_visualtype(x0 : Pointer(Connection),x1 : Int32, x2 : Visualid) : Pointer(Visualtype)
+	fun xcb_aux_get_visualtype(x0 : Pointer(Connection),x1 : Int32, x2 : Visualid) : Pointer(Visualtype)
 
-fun xcb_aux_find_visual_by_id(x0 : Pointer(Screen), x1 : Visualid) : Pointer(Visualtype)
+	fun xcb_aux_find_visual_by_id(x0 : Pointer(Screen), x1 : Visualid) : Pointer(Visualtype)
 
-fun xcb_aux_find_visual_by_attrs(x0 : Pointer(Screen), x1 : Int8, x2 : Int8) : Pointer(Visualtype)
+	fun xcb_aux_find_visual_by_attrs(x0 : Pointer(Screen), x1 : Int8, x2 : Int8) : Pointer(Visualtype)
 
-fun xcb_aux_sync(x0 : Pointer(Connection)) : Void
+	fun xcb_aux_sync(x0 : Pointer(Connection)) : Void
 
-struct ParamsCW 
-	back_pixmap : UInt32 
-	back_pixel : UInt32 
-	border_pixmap : UInt32 
-	border_pixel : UInt32 
-	bit_gravity : UInt32 
-	win_gravity : UInt32 
-	backing_store : UInt32 
-	backing_planes : UInt32 
-	backing_pixel : UInt32 
-	override_redirect : UInt32 
-	save_under : UInt32 
-	event_mask : UInt32 
-	dont_propagate : UInt32 
-	colormap : UInt32 
-	cursor : UInt32 
-end 
+	struct ParamsCW 
+		back_pixmap : UInt32 
+		back_pixel : UInt32 
+		border_pixmap : UInt32 
+		border_pixel : UInt32 
+		bit_gravity : UInt32 
+		win_gravity : UInt32 
+		backing_store : UInt32 
+		backing_planes : UInt32 
+		backing_pixel : UInt32 
+		override_redirect : UInt32 
+		save_under : UInt32 
+		event_mask : UInt32 
+		dont_propagate : UInt32 
+		colormap : UInt32 
+		cursor : UInt32 
+	end 
 
-fun xcb_aux_create_window(x0 : Pointer(Connection), x1 : UInt8, x2 : Window, x3 : Window, x4 : Int32, x5 : Int32, x6 : UInt16, x7 : UInt16, x8 : UInt16, x9 : UInt16, x10 : Visualid, x11 : UInt32, x12 : Pointer(ParamsCW)) : VoidCookie  
+	fun xcb_aux_create_window(x0 : Pointer(Connection), x1 : UInt8, x2 : Window, x3 : Window, x4 : Int32, x5 : Int32, x6 : UInt16, x7 : UInt16, x8 : UInt16, x9 : UInt16, x10 : Visualid, x11 : UInt32, x12 : Pointer(ParamsCW)) : VoidCookie  
 
-fun xcb_aux_create_window_checked(x0 : Pointer(Connection), x1 : UInt8, x2 : Window, x3 : Window, x4 : Int32, x5 : Int32, x6 : UInt16, x7 : UInt16, x8 : UInt16, x9 : UInt16, x10 : Visualid, x11 : UInt32, x12 : Pointer(ParamsCW)) : VoidCookie
+	fun xcb_aux_create_window_checked(x0 : Pointer(Connection), x1 : UInt8, x2 : Window, x3 : Window, x4 : Int32, x5 : Int32, x6 : UInt16, x7 : UInt16, x8 : UInt16, x9 : UInt16, x10 : Visualid, x11 : UInt32, x12 : Pointer(ParamsCW)) : VoidCookie
 
-fun xcb_aux_change_window_attributes(x0 : Pointer(Connection), x1 : Window, x2 : UInt32, x3 : Pointer(ParamsCW)) : VoidCookie
+	fun xcb_aux_change_window_attributes(x0 : Pointer(Connection), x1 : Window, x2 : UInt32, x3 : Pointer(ParamsCW)) : VoidCookie
 
-struct ParamsConfigureWindow
-	x : Int32   
-	y : Int32   
-	width : UInt32 
-	height : UInt32 
-	border_width : UInt32 
-	sibling : UInt32 
-	stack_mode : UInt32 
+	struct ParamsConfigureWindow
+		x : Int32   
+		y : Int32   
+		width : UInt32 
+		height : UInt32 
+		border_width : UInt32 
+		sibling : UInt32 
+		stack_mode : UInt32 
+	end
+
+	fun xcb_aux_configure_window(x0 : Pointer(Connection), x1 : Window, x2 : UInt16, x3 : Pointer(ParamsConfigureWindow)) : VoidCookie
+
+	struct ParamsGC
+		function : UInt32 
+		plane_mask : UInt32 
+		foreground : UInt32 
+		background : UInt32
+		line_width : UInt32
+		line_style : UInt32
+		cap_style : UInt32
+		join_style : UInt32
+		fill_style : UInt32
+		fill_rule : UInt32
+		tile : UInt32
+		stipple : UInt32
+		tile_stipple_origin_x : UInt32
+		tile_stipple_origin_y : UInt32
+		font : UInt32
+		subwindow_mode : UInt32
+		graphics_exposures : UInt32
+		clip_originX : UInt32
+		clip_originY : UInt32
+		mask : UInt32
+		dash_offset : UInt32 
+		dash_list : UInt32
+		arc_mode : UInt32
+	end
+
+	fun xcb_aux_create_gc(x0 : Pointer(Connection), x1 : GContext, x2 : Drawable, x3 : UInt32, x4 : Pointer(ParamsGC)) : VoidCookie
+
+
+	fun xcb_aux_create_gc_checked (x0 : Pointer(Connection), x1 : GContext, x2 : Drawable, x3 : UInt32, x4 : Pointer(ParamsGC)) : VoidCookie
+
+	fun xcb_aux_change_gc(x0 : Pointer(Connection), x1 : GContext, x2 : UInt32, x3 : Pointer(ParamsGC)) : VoidCookie
+	fun xcb_aux_change_gc_checked (x0 : Pointer(Connection), x2 : GContext, x3 : UInt32, x4 : Pointer(ParamsGC)) : VoidCookie
+
+	struct ParamsKeyboard
+		key_click_percent : UInt32 
+		bell_percent : UInt32 
+		bell_pitch : UInt32 
+		bell_duration : UInt32 
+		led : UInt32 
+		led_mode : UInt32 
+		key :  UInt32 
+		auto_repeat_mode :  UInt32 
+	end 
+
+	fun xcb_aux_change_keyboard_control(x0 : Pointer(Connection), x1 : UInt32, x2 : Pointer(ParamsKeyboard)) : VoidCookie
+
+	fun xcb_aux_parse_color(x0 : Pointer(Char), x1 : Pointer(UInt16), x2 : Pointer(UInt16), Pointer(UInt16)) : Int32
+
+	fun xcb_aux_set_line_attributes_checked (PoInt32er(Connection), GContext, UInt16, Int32, Int32, Int32) :  VoidCookie
+
+	fun xcb_aux_clear_window(x0 : Pointer(Connection), x1 : Window) : VoidCookie
 end
-
-fun xcb_aux_configure_window(x0 : Pointer(Connection), x1 : Window, x2 : UInt16, x3 : Pointer(ParamsConfigureWindow)) : VoidCookie
-
-struct ParamsGC
-	function : UInt32 
-	plane_mask : UInt32 
-	foreground : UInt32 
-	background : UInt32
-	line_width : UInt32
-	line_style : UInt32
-	cap_style : UInt32
-	join_style : UInt32
-	fill_style : UInt32
-	fill_rule : UInt32
-	tile : UInt32
-	stipple : UInt32
-	tile_stipple_origin_x : UInt32
-	tile_stipple_origin_y : UInt32
-	font : UInt32
-	subwindow_mode : UInt32
-	graphics_exposures : UInt32
-	clip_originX : UInt32
-	clip_originY : UInt32
-	mask : UInt32
-	dash_offset : UInt32 
-	dash_list : UInt32
-	arc_mode : UInt32
-end
-
-fun xcb_aux_create_gc(x0 : Pointer(Connection), x1 : GContext, x2 : Drawable, x3 : UInt32, x4 : Pointer(ParamsGC)) : VoidCookie
-
-
-fun xcb_aux_create_gc_checked (x0 : Pointer(Connection), x1 : GContext, x2 : Drawable, x3 : UInt32, x4 : Pointer(ParamsGC)) : VoidCookie
-
-fun xcb_aux_change_gc(x0 : Pointer(Connection), x1 : GContext, x2 : UInt32, x3 : Pointer(ParamsGC)) : VoidCookie
-fun xcb_aux_change_gc_checked (x0 : Pointer(Connection), x2 : GContext, x3 : UInt32, x4 : Pointer(ParamsGC)) : VoidCookie
-
-struct ParamsKeyboard
-	key_click_percent : UInt32 
-	bell_percent : UInt32 
-	bell_pitch : UInt32 
-	bell_duration : UInt32 
-	led : UInt32 
-	led_mode : UInt32 
-	key :  UInt32 
-	auto_repeat_mode :  UInt32 
-end 
-
-fun xcb_aux_change_keyboard_control(x0 : Pointer(Connection), x1 : UInt32, x2 : Pointer(ParamsKeyboard)) : VoidCookie
-
-fun xcb_aux_parse_color(x0 : Pointer(Char), x1 : Pointer(UInt16), x2 : Pointer(UInt16), Pointer(UInt16)) : Int32
-
-fun xcb_aux_set_line_attributes_checked (PoInt32er(Connection), GContext, UInt16, Int32, Int32, Int32) :  VoidCookie
-
-fun xcb_aux_clear_window(x0 : Pointer(Connection), x1 : Window) : VoidCookie
