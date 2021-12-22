d9e5af8f57cf7cee4f8d4592e0fb4cb11aaa7d14
Hampus Andreas Niklas Rosencrantz <hamros@tuta.io>
2021-12-22 16:13:48 +0100

    fonts

--- font.cr
+++ font.cr
@@ -31,7 +31,7 @@ end
 def draw_text_pango(text, text_len, drawable, surface, x, y, max_width, pango_markup)
 	cr = cairo_create(surface)
 	layout = create_layout_with_dpi(cr)
-	pango_layout_set_font_description(layout, savedFont.specific.pango_desc)
+	pango_layout_set_font_description(layout, saved_font.specific.pango_desc)
 	pango_layout_set_width(layout, max_width * PANGO_SCALE)
 	pango_layout_set_wrap(layout, PANGO_WRAP_CHAR)
 	pango_layout_set_ellipsize(layout, PANGO_ELLIPSIZE_END)
@@ -45,7 +45,7 @@ def draw_text_pango(text, text_len, drawable, surface, x, y, max_width, pango_ma
 	pango_cairo_update_layout(cr, layout)
 	height = 0
 	pango_layout_get_pixel_size(layout, nil, pointerof(height))
-	yoffset = (height - savedFont.height) / 2
+	yoffset = (height - saved_font.height) / 2
 	cairo_move_to(cr, x, y - yoffset)
 	pango_cairo_show_layout(cr, layout)
 	g_object_unref(layout)
@@ -57,7 +57,7 @@ def predict_text_width_pango(text, text_len, pango_markup)
 	cr = cairo_create(surface)
 	layout = create_layout_with_dpi(cr)
 	width = 0
-	pango_layout_set_font_description(layout, savedFont.specific.pango_desc)
+	pango_layout_set_font_description(layout, saved_font.specific.pango_desc)
 	if pango_markup
 		pango_layout_set_markup(layout, text, text_len)
 	else
@@ -72,4 +72,150 @@ def predict_text_width_pango(text, text_len, pango_markup)
 end
 
 def load_font(pattern, fallback)
+	font.type = FONT_TYPE_NONE
+	pattern = nil
+
+	return font if !conn
+
+	if pattern.size > "pango:".size && !"pango:".compare(pattern)
+		font_pattern = pattern + "xft:"
+		if load_pango_font(pointerof(font), font_pattern)
+			font.pattern = pattern.dup
+			return font
+		end
+	end
+
+	font.specific.xcb.id = xcb_generate_id(conn)
+
+	font_cookie = xcb_open_font_checked(conn, font.specific.xcb.id, pattern.size, pattern)
+	info_cookie = xcb_query_font(conn, font.specific.xcb.id)
+
+	error = xcb_request_check(conn, font_cookie)
+	if fallback && error
+		puts "Could not open font #{pattern} (X error #{error.error_code}). Trying fallback to 'fixed'."
+
+		pattern = "fixed"
+		font_cookie = xcb_open_font_checked(conn, font.specific.xcb.id, pattern.size, pattern)
+		info_cookie = xcb_query_font(conn, font.specific.xcb.id)
+
+		error = xcb_request_check(conn, font_cookie)
+		if error
+			puts "Could not open fallback font 'fixed', trying with '-misc-*'"
+
+			pattern = "-misc-*"
+			font_cookie = xcb_open_font_checked(conn, font.specific.xcb.id, pattern.size, pattern)
+			info_cookie = xcb_query_font(conn, font.specific.xcb.id)
+
+			if error = xcb_request_check(conn, font_cookie)
+				puts "Could open neither requested font nor fallbacks (fixed or -misc-*): X11 error #{error.error_code}"
+				exit
+			end 
+		end
+	end
+
+	font.pattern = pattern.dup
+	puts "Using X font #{pattern}"
+
+	if !font.specific.xcb.info = xcb_query_font_reply(conn, info_cookie, nil)
+		puts "Could not load font #{pattern}"
+		exit
+	end
+
+	if !xcb_query_font_char_infos_length(font.specific.xcb.info)
+		font.specific.xcb.table = nil
+	else
+		font.specific.xcb.table = xcb_query_font_char_infos(font.specific.xcb.info)
+	end
+
+	font.height = font.specific.xcb.info.font_ascent + font.specific.xcb.info.font_descent
+	font.type = FONT_TYPE_XCB
+	return font
+end
+
+def set_font_colors(gc, foreground, background)
+	case saved_font.type
+	when FONT_TYPE_NONE
+		break
+	when FONT_TYPE_XCB
+		mask = XCB_GC_FOREGROUND | XCB_GC_BACKGROUND | XCB_GC_FONT
+		values = [foreground.colorpixel, background.colorpixel, saved_font.specific.xcb.id]
+		xcb_change_gc(conn, gc, mask, values)
+	when FONT_TYPE_PANGO
+		pango_font_red = foreground.red
+		pango_font_green = foreground.green
+		pango_font_blue = foreground.blue
+		break
+	end
+end
+
+def font_is_pango?
+	return saved_font.type == FONT_TYPE_PANGO
+end
+
+def draw_text_xcb(text, text_len, drawable, gc, x, y)
+	pos_y = y + saved_font.specific.xcb.info.font_ascent
+	offset = 0
+	loop do
+		chunk_size = text_len > 255 ? 255 : text_len
+		chunk = (text + offset)
+		xcb_image_text_16(conn, chunk_size, drawable, gc, x, pos_y, chunk)
+		offset += chunk_size
+		text_len -= chunk_size
+		break if text_len == 0
+		x += predict_text_width_xcb(chunk, chunk_size)
+	end
+end
+
+def draw_text(text, drawable, gc, surface, x, y, max_width)
+	case saved_font.type
+	when FONT_TYPE_NONE
+		return
+	when FONT_TYPE_XCB
+		draw_text_xcb(text, string_get_num_glyphs(text), drawable, gc, x, y)
+		break
+	when FONT_TYPE_PANGO
+		draw_text_pango(string_as_utf8(text), string_get_num_bytes(text), drawable, surface, x, y, max_width, string_is_markup(text))
+		return
+	end
+end
+
+def xcb_query_text_width(input, text_len)
+	return if !text_len
+	width = 0
+	if !saved_font.specific.xcb.table
+		width = xcb_query_text_width(input, text_len)
+	else
+		font_info = saved_font.specific.xcb.info
+		font_table = saved_font.specific.xcb.table
+		width = 0
+		text_len.times do |i|
+			row = input[i].byte1
+			col = input[i].byte2
+			if row < font_info.min_byte1 ||
+				 row > font_info.max_byte1 ||
+				 col < font_info.min_char_or_byte2 ||
+				 col > font_info.max_char_or_byte2
+				next
+			end
+			if !(info.character_width != 0 ||
+				 (info.right_side_bearing | 
+				 info.left_side_bearing |
+				 info.ascent |
+				 info.descent))
+			width += info.character_width
+			end
+		end
+		return width
+	end
+end
+
+def predict_text_width(text)
+	case saved_font.type
+	when FONT_TYPE_NONE
+		return 0
+	when FONT_TYPE_XCB
+		return predict_text_width_xcb(string_as_ucs2(text), string_get_num_glyphs(text))
+	when FONT_TYPE_PANGO
+		return predict_text_width_pango(string_as_utf8(text), string_get_num_bytes(text), string_is_markup(text))
+	end
 end
