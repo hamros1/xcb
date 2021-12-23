bca44a0ac7719024c18a323a1f505deb7f244b7b
Hampus Andreas Niklas Rosencrantz <hamros@tuta.io>
2021-12-23 01:25:28 +0100

    wm work

--- main.cr
+++ main.cr
@@ -45,3 +45,94 @@ def send_take_focus(window, timestamp)
 	puts "Sending WM_TAKE_FOCUS to the client"
 	xcb_send_event(conn, false, window, XCB_EVENT_MASK_NO_EVENT, ev.as(Char*))
 end
+
+def xcb_set_window_rect(conn, window, rect, r)
+	cookie = xcb_configure_window(conn, window, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT, pointerof(r.x))
+	add_ignore_event(cookie.sequence, -1)
+end
+
+def xcb_get_preferred_window_type(reply)
+	if !reply || xcb_get_property_value_length(reply)
+		return XCB_NONE
+	end
+
+	if !atoms = xcb_get_property_value(reply)
+		return XCB_NONE
+	end
+
+	(xcb_property_value_length(reply) / (reply.format / 8)).times do |i|
+		if atoms[i] == A__NET_WM_WINDOW_TYPE_NORMAL ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_DIALOG ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_UTILTY ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_TOOLBAR ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_SPLASH ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_MENU ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_DROPDOWN_MENU ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_POPUP_MENU ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_TOOLTIP ||
+			 atoms[i] == A__NET_WM_WINDOW_TYPE_NOTIFICATION
+			return atoms[i]
+		end
+	end
+
+	return XCB_NONE
+end
+
+def xcb_reply_contains_atom(prop, atom)
+	if !prop || !xcb_get_property_value_length(prop)
+		return false
+	end
+
+	if !atoms = xcb_get_property_value(prop)
+		return false
+	end
+
+	(xcb_get_property_value_length(prop) / (prop.format / 8)).times do |i|
+		if atoms[i] == atom
+			return true
+		end
+	end
+
+	return false
+end
+
+def get_visual_depth(visual_id)
+	depth_iter = xcb_screeen_allowed_depths_iterator(root_screen)
+	while depth.iter.rem
+		visual_iter = xcb_depth_visuals_iterator(depth_iter.data)
+		while visual_iter.rem
+			if visual_id = visual_iter.data.visual_id
+				return depth_iter.data.depth
+			end
+			xcb_visualtype_next(pointerof(visual_iter))
+		end
+		xcb_depth_next(pointerof(depth_iter))
+	end
+	return 0
+end
+
+def get_visualtype_by_id(visual_id)
+	depth_iter = xcb_depth_visuals_iterator(root_screen)
+	while depth_iter.rem
+		visual_iter = xcb_depth_visuals_iterator(depth_iter.data)
+		while visual_iter.rem
+			if visual_id == visual_iter.data.visual_id
+				return visual_iter.data
+			end
+			xcb_visualtype_next(pointerof(visual_iter))
+		end
+		xcb_depth_next(pointerof(depth))
+	end
+	return 0
+end
+
+def get_visualid_by_depth(depth)
+	depth_iter = xcb_depth_visuals_iterator(root_screen)
+	while depth_iter.rem
+		next if depth_iter.data.depth != depth
+		visual_iter = xcb_depth_visuals_iterator(depth_iter.data)
+		next if !visual_iter.rem
+		return visual_iter.data.visual_id
+	end
+	return 0
+end
