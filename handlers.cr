ecb82457511da16a7b5dba9fe3243acb2fffc56b
Hampus Andreas Niklas Rosencrantz <hamros@tuta.io>
2021-12-24 04:39:07 +0100

    xkb binding

--- handlers.cr
+++ handlers.cr
@@ -75,8 +75,8 @@ def handle_motion_notify(event)
 end
 
 def handle_mapping_notify(event)
-	return if event.request != XCB_MAPPING_KEYBOARD &&
-		 event.request != XCB_MAPPING_MODIFIER
+	return if event.request != XCB_MAPPINGKeyboard &&
+		event.request != XCB_MAPPING_MODIFIER
 	puts "Received mapping_notify for keyboard or modifier mapping, re-grabbing keys"
 	xcb_refresh_keyboard_mapping(keysyms, event)
 	xcb_numlock_mask = aio_get_mask_for(XCB_NUM_LOCK, keysyms)
@@ -105,13 +105,13 @@ def handle_configure_request(event)
 		puts "Configure request for unmanaged window, can do that"
 		mask = 0
 		c = 0
-		copy_mask_member(XCB_CONFIG_WINDOW_X, x)
-		copy_mask_member(XCB_CONFIG_WINDOW_Y, y)
-		copy_mask_member(XCB_CONFIG_WINDOW_WIDTH, width)
-		copy_mask_member(XCB_CONFIG_WINDOW_HEIGHT, height)
-		copy_mask_member(XCB_CONFIG_WINDOW_BORDER_WIDTH, border_width)
-		copy_mask_member(XCB_CONFIG_WINDOW_SIBLING, sibling)
-		copy_mask_member(XCB_CONFIG_WINDOW_STACK_MODE, stack_mode)
+		copy_mask_member(XCB_CONFIGWindow_X, x)
+		copy_mask_member(XCB_CONFIGWindow_Y, y)
+		copy_mask_member(XCB_CONFIGWindow_WIDTH, width)
+		copy_mask_member(XCB_CONFIGWindow_HEIGHT, height)
+		copy_mask_member(XCB_CONFIGWindow_BORDER_WIDTH, border_width)
+		copy_mask_member(XCB_CONFIGWindow_SIBLING, sibling)
+		copy_mask_member(XCB_CONFIGWindow_STACK_MODE, stack_mode)
 		xcb_configure_window(conn, event.window, mask, values)
 		xcb_flush(conn)
 		return
@@ -127,39 +127,39 @@ def handle_configure_request(event)
 	end
 	floatingcon = con.parent
 	newrect = floatingcon.rect
-	if event.value_mask & XCB_CONFIG_WINDOW_X
+	if event.value_mask & XCB_CONFIGWindow_X
 		newrect.x = event.x + (-1) * bsr.x
 		puts "proposed x = #{event.x}, new x is #{newrect.x}"
 	end
-	if event.value_mask & XCB_CONFIG_WINDOW_Y
+	if event.value_mask & XCB_CONFIGWindow_Y
 		newrect.y = event.y + (-1) * bsr.y
 		puts "proposed x = #{event.y}, new x is #{newrect.y}"
 	end
-	if event.value_mask & XCB_CONFIG_WINDOW_WIDTH
+	if event.value_mask & XCB_CONFIGWindow_WIDTH
 		newrect.width = event.width + (-1) * bsr.width
 		newrect.width = con.border_width * 2
 		puts "proposed width = #{event.width}, new width is #{newrect.width} (x11 border #{con.border_width})"
 	end
-	if event.value_mask & XCB_CONFIG_WINDOW_HEIGHT
+	if event.value_mask & XCB_CONFIGWindow_HEIGHT
 		newrect.width = event.height + (-1) * bsr.height
 		newrect.width = con.border_width * 2
 		puts "proposed width = #{event.height}, new width is #{newevent.height} (x11 border #{con.border_width})"
 	end
-	
+
 	puts "Container is a floating leaf node, will do that"
 	floating_reposition(floatingcon, newrect)
 	return
 
 	if con.parent && con.parent.type == CT_DOCKAREA
 		puts "Reconfiguring dock window (con = #{con})"
-		if event.value_mask & XCB_CONFIG_WINDOW_HEIGHT
+		if event.value_mask & XCB_CONFIGWindow_HEIGHT
 			puts "Dock client wants to change height to #{event.height} we can do that"
 			con.geometry.height = event.height
 			tree_render
 		end
-		if event.value_mask & XCB_CONFIG_WINDOW_X || event.value_mask & XCB_CONFIG_WINDOW_Y
-			x = event.value_mask & XCB_CONFIG_WINDOW_X ? event.x : con.geometry.x
-			y = event.value_mask & XCB_CONFIG_WINDOW_Y ? event.y : con.geometry.y
+		if event.value_mask & XCB_CONFIGWindow_X || event.value_mask & XCB_CONFIG_WINDOW_Y
+			x = event.value_mask & XCB_CONFIGWindow_X ? event.x : con.geometry.x
+			y = event.value_mask & XCB_CONFIGWindow_Y ? event.y : con.geometry.y
 			current_output = con_get_output(con)
 			target = get_output_containing(x, y)
 			if target && current_output != target.con
@@ -176,7 +176,7 @@ def handle_configure_request(event)
 		fake_absolute_configure_notify(con)
 	end
 
-	if event.value_mask & XCB_CONFIG_WINDOW_STACK_MODE
+	if event.value_mask & XCB_CONFIGWindow_STACK_MODE
 		puts "window #{event.window} wants to be stacked #{event.stack_mode}"
 		if event.stack_mode != XCB_STACK_MODE_ABOVE
 			puts "stack_mode != XCB_STACK_MODE_ABOVE, ignoring ConfigureRequest"
@@ -250,9 +250,132 @@ def handle_unmap_notify_event(event)
 	end
 	xcb_delete_property(conn, event.window, A__NET_WM_DESKTOP)
 	xcb_delete_property(conn, event.window, A__NET_WM_STATE)
-	tree_close_internal(con, DONT_KILL_WINDOW, false)
+	tree_close_internal(con, DONT_KILLWindow, false)
 	tree_render
 end
 
 def handle_destroy_notify_event(event)
+	puts "destroy notify for #{event.event}, #{event.window}"
+	unmap = xcb_unmap_notify_event.new(
+		sequence: event.sequence,
+		event: event.event,
+		window: event.window
+	)
+	handle_unmap_notify_event(unmap)
+end
+
+def window_name_changed(window, old_name)
+	return false if !old_name && !window.name
+	return true if !old_name ^ !window.name
+	return (old_name.compare(window.name) != 0)
+end
+
+def handle_window_name_change(con, prop)
+	old_name = con.window.name ? str_as_utf8(con.window.name) : nil
+	window_update_name(con.window, prop)
+	con = remanage_window(con)
+	x_push_changes(croot)
+	if window_name_changed(con.window, old_name)
+		ipc_send_window_event("title", con)
+	end
+	return true
+end
+
+def hadle_window_name_change_legacy(con, prop)
+	old_name = con.window.name ? str_as_utf8(con.window.name) : nil
+	window_update_name_legacy(con.window, prop)
+	con = remanage_window(croot)
+	x_push_changes(croot)
+	if window_name_changed(con.window, old_name)
+		ipc_send_window_event("title", con)
+	end
+	return true
+end
+
+def handle_windowrole_change(con, prop)
+	window_update_role(con.window, prop)
+	con = remanage_window(con)
+	return true
+end
+
+def handle_expose_event(event)
+	puts "window = #{event.window}"
+	if parent = con_by_frame_id(event.window)
+		puts "expose event for unknown window, ignoring"
+		return
+	end
+	draw_util_copy_surface(parent.frame_buffer, parent.frame, 0, 0, 0, 0, parent.rect.width, parent.rect.height)
+	xcb_flush(conn)
+end
+
+NetWmMoveResizeSizeTopLeft = 0
+NetWmMoveResizeSizeTop = 1
+NetWmMoveResizeSizeTopRight = 2
+NetWmMoveResizeSize_Right = 3
+NetWmMoveResizeSizeBottomRight = 4
+NetWmMoveResizeSizeBottom = 5
+NetWmMoveResizeSizeBottomLeft = 6
+NetWmMoveResizeSizeLeft = 7
+NetWmMoveResizeMove = 8
+NetWmMoveResizeSizeKeyboard = 9
+NetWmMoveResizeMoveKeyboard = 10
+NetWmMoveResizeCancel = 11
+
+NetMoveResizeWindowX = (1 << 8)
+NetMoveResizeWindowY = (1 << 9)
+NetMoveResizeWindowWidth = (1 << 10)
+NetMoveResizeWindowHeight = (1 << 11)
+
+def handle_client_message(event)
+	if sn_xcb_display_process_event(sndisplay, event.window)
+		return
+	end
+	puts "ClientMessage for window #{event.window}"
+	if event.type == NetWmState
+		if event.format != 32 ||
+				(event.data.data32[1] != NetWmStateFullscreen &&
+		 event.data.data32[1] != NetWmStateDemandsAttention &&
+		 event.data.data32[1] != NetWmStateSticky)
+		puts "Unknown atom in clientmessage of type #{event.data.data32[1]}"
+		return
+		end
+		con = con_by_window_id(event.window)
+		if !con
+			puts "Could not get window for client message"
+			return
+		end
+		if event.data.data32[1] == NetWmStateFullscreen
+			if con.fullscreen_mode != CF_NONE &&
+					(event.data.data32[0] == NetWmStateRemove ||
+			event.data.data32[0] == NetWmStateToggle) ||
+			con.fullscreen_mode == CF_NONE &&
+			(event.data.data32[0] == NetWmStateAdd ||
+		event.data.data32[0] == NetWmStateToggle)
+			puts "Toggling fullscreen"
+			con_toggle_fullscreen
+			end
+		else if event.data.data32[1] == NetWmStateDemandsAttention
+			if event.data.data32[0] == NetWmStateAdd
+				con_set_urgency(con, true)
+			else if event.data.data32[0] == NetWmStateRemove
+				con_set_urgency(con, false)
+			else if event.data.data32[0] == NetWmStateToggle
+				con_set_urgency(con, !con.urgent)
+			end
+		else if event.data.data32[1] == NetWmStateSticky
+			if event.data.data32[0] == NetWmStateAdd
+				con.sticky = true
+			else if event.data.data32[0] == NetWmStateRemove
+				con.sticky = false
+			else if event.data.data32[0] == NetWmStateToggle
+				con.sticky = !con.sticky
+			end
+			puts "New sticky status for con = #{con} is #{con.sticky}"
+			ewmh_update_sticky(con.window.id, con.sticky)
+			output_push_sticky_windows(focused)
+			ewmh_update_wm_desktop
+		end
+		tree_render
+	else if event.type == NetActiveWindow
+	end
 end
